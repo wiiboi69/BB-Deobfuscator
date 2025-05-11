f1f355250115a4999e504b627bc76a59702d9689
wiiboi69 <meereaareea@gmail.com>
2025-05-11 19:45:35 +0100

    t

---
+++ BB_Deob/BB_Deob/Mappings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BB_Deob
+{
+    public class Mappings
+    {
+        public static bool Read_mapping(string path)
+        {
+            try
+            {
+                module_map = JsonConvert.DeserializeObject<Module_mapping>(File.ReadAllText(path));
+                return true;
+            }
+            catch {}
+            return false;
+        }
+        public static Module_mapping module_map;
+        public static string Get_class_name(string name)
+        {
+            foreach (Class_mapping item in module_map.Class)
+            {
+                if (item.obfuscated_name == name)
+                {
+                    return item.name;
+                }
+            }
+            return null;
+        }
+        public class Module_mapping
+        {
+            public string File_hash;
+            public List<Class_mapping> Class = new List<Class_mapping>();
+        }
+        public class Method_mapping
+        {
+            public string obfuscated_name;
+            public string name;
+            /// <summary>
+            /// the key is the obfuscated_name, the value is the name
+            /// </summary>
+            public Dictionary<string, string> Param;
+        }
+        public class Field_mapping
+        {
+            public string obfuscated_name;
+            public string name;
+        }
+        public class Class_mapping
+        {
+            public string obfuscated_name;
+            public string name;
+            public List<Method_mapping> Methods = new List<Method_mapping>();
+            public List<Field_mapping> Fields = new List<Field_mapping>();
+            public List<Field_mapping> Properties = new List<Field_mapping>();
+        }
+    }
+}
--- BB_Deob/BB_Deob/Program.cs
+++ BB_Deob/BB_Deob/Program.cs
@@ -14,18 +14,31 @@ namespace BB_Deob
     {
         static void Main(string[] args)
         {
-            ModuleDef module;
+            string mapping_path = "";
+            ModuleDef module;   
             try
             {
                 module = ModuleDefMD.Load(args[0]);
+                mapping_path = args[1];
             }
             catch
             {
                 Console.WriteLine("No arguments added!");
                 return;
             }
+            if (!Mappings.Read_mapping(mapping_path))
+            {
+                Console.WriteLine("the mapping failed to load.");
+                return;
+            }
+
+
             Console.WriteLine("Loaded module " + module.Name + ", Runtime: " + module.RuntimeVersion);
             string bb_version = "unknown/bb obfuscator not found";
+
+            Console.WriteLine($"bb_mapper: Loading mapping json from {mapping_path}...");
+
+            // Note: some BB Obfuscator Version may mest around some more stuff like enum name
             foreach (TypeDef type in module.Types)
             {
                 if (type.Name.Contains("__BB_OBFUSCATOR_VERSION_"))
@@ -33,44 +46,74 @@ namespace BB_Deob
                     bb_version = type.Name.String.Split('_')[5] + "." + type.Name.String.Split('_')[6] + "." + type.Name.String.Split('_')[7];
                 }
             }
+            Console.WriteLine("some BB Obfuscator Version may mest around some more stuff like enum name");
             Console.WriteLine("BB Obfuscator Version: " + bb_version);
-            Console.WriteLine("[1] Renaming...");
+            Console.WriteLine("bb_renamer: Renaming...");
             int count = 0;
             int classNumber = 0;
-            Regex regex = new Regex(@"[a-zA-Z0-9_]+");
+            Regex regex = new Regex(@"^[A-Z]{11}$");
             foreach (TypeDef type in module.Types)
             {
                 if (!type.Name.Contains("__BB_OBFUSCATOR_VERSION_") || !type.Name.Contains("ArrayCopy"))
                 {
                     if (type.Name != "<Module>")
                     {
-                        if (!regex.IsMatch(type.Name))
+                        string temp = null;
+                        if (regex.IsMatch(type.Name))
                         {
-                            type.Name = "Class" + classNumber;
-                            ++classNumber;
+                            temp = Mappings.Get_class_name(type.Name);
+                            if (string.IsNullOrEmpty(temp))
+                            {
+                                type.Name = "Class" + classNumber;
+                                ++classNumber;
+                            }
+                            else
+                                type.Name = temp;
                         }
                         int fieldNumber = 0;
+                        int PropertieNumber = 0;
                         foreach (FieldDef field in type.Fields)
                         {
-                            if (!regex.IsMatch(field.Name))
+                            if (regex.IsMatch(field.Name))
                             {
                                 field.Name = "Field" + fieldNumber;
                                 ++fieldNumber;
                             }
                         }
-                        int paramNumber = 0;
+                        foreach (PropertyDef field in type.Properties)
+                        {
+                            if (regex.IsMatch(field.Name))
+                            {
+                                field.Name = "Properties" + PropertieNumber;
+                                ++PropertieNumber;
+                            }
+                        }
                         int methodNumber = 0;
                         foreach (MethodDef method in type.Methods)
                         {
+                            int paramNumber = 0;
                             foreach (ParamDef param in method.ParamDefs)
                             {
-                                if (!regex.IsMatch(param.Name))
+                                if (regex.IsMatch(param.Name))
                                 {
                                     param.Name = "Param" + paramNumber;
                                     ++paramNumber;
                                 }
                             }
-                            if (!regex.IsMatch(method.Name))
+                            if (regex.IsMatch(method.Name)
+                                && "Initialize" != method.Name
+                                && "OnDisable" != method.Name
+                                && "OnDestroy" != method.Name
+                                && "OnEnable" != method.Name
+                                && "OnPostRender" != method.Name
+                                && "Awake" != method.Name
+                                && "Start" != method.Name
+                                && "Update" != method.Name
+                                && "FixedUpdate" != method.Name
+                                && "LateUpdate" != method.Name
+                                && "OnGUI" != method.Name
+                                && "OnCollisionEnter" != method.Name
+                                && "LateUpdate" != method.Name)
                             {
                                 method.Name = "Method" + methodNumber;
                                 ++methodNumber;
@@ -79,13 +122,12 @@ namespace BB_Deob
                     }
                 }
                 count++;
-                Console.WriteLine("[1] Renaming [" + count + "/" + module.Types.Count + "]");
+                Console.WriteLine("bb_renamer: Renaming [" + count + "/" + module.Types.Count + "]");
             }
-            Console.WriteLine("[2] Watermarking...");
-            module.Name += "_renamed";
-            Console.WriteLine("[3] Saving...");
-            module.Write(module.FullName.Split('.')[0] + "-Renamed.dll");
-            Console.WriteLine("Saved to " + Directory.GetCurrentDirectory() + @"\" + module.FullName.Split('.')[0] + "-Renamed.dll");
+            Console.WriteLine("bb_file: Saving...");
+            string out_file_name = module.FullName.Split('.')[0] + "-Renamed.dll";
+            module.Write(out_file_name);
+            Console.WriteLine("bb_file: Saved to " + Directory.GetCurrentDirectory() + @"\" + out_file_name);
         }
     }
 }
